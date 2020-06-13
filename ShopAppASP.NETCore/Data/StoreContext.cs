fd1d6b6b7c93f29110b40af651a4c9197cfd9d37
Artem Dudar <Artem Dudar@DESKTOP-L5MAP66>
2020-06-14 00:46:59 +0300

    added entity framework core and storecontext

---
+++ ShopAppASP.NETCore/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopAppASP.NETCore.Entities;
+
+namespace ShopAppASP.NETCore.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
