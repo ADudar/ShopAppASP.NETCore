bc8a7500b17fb4e74b25f92504d2f857d69dec5e
Artem Dudar <Artem Dudar@DESKTOP-L5MAP66>
2020-06-14 23:14:52 +0300

    configure automapper

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -33,11 +33,11 @@ namespace API.Controllers
         }
     
         [HttpGet]
-        public async Task<ActionResult<List<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
         {
             var spec = new ProductsWithTypesAndBrandsSpecification();
             var products = await _productRepo.ListAsync(spec);
-            return products.Select(product => _mapper.Map<Product, ProductToReturnDto>(product)).ToList();
+            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
         }
 
         [HttpGet("{id}")]
