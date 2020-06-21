03c1a46e9c9f83a73bcd646fbb66ad322df92ad8
Artem Dudar <Artem Dudar@DESKTOP-L5MAP66>
2020-06-21 22:12:46 +0300

    created product, pagination interfaces

--- API/ClientApp/src/app/app.component.ts
+++ API/ClientApp/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {IProduct} from './models/product';
+import {IPagination} from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  products;
+  products: IProduct[];
 
   constructor(private http: HttpClient) {
   }
@@ -14,7 +16,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.http.get('https://localhost:5001/api/products?pageSize=50')
-      .subscribe((response: any) => {
+      .subscribe((response: IPagination) => {
         this.products = response.data;
       }, e => console.error(e));
   }
