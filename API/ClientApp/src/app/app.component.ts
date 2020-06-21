438cb1da7304b1cfe0fefb7b94758dab91a360ca
Artem Dudar <Artem Dudar@DESKTOP-L5MAP66>
2020-06-21 22:02:14 +0300

    fetch products

--- API/ClientApp/src/app/app.component.ts
+++ API/ClientApp/src/app/app.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  products;
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50')
+      .subscribe((response: any) => {
+        this.products = response.data;
+      }, e => console.error(e));
+  }
+
 }
