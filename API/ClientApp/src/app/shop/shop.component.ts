f20c6471b059387085efc6cb1d2c4bc9fbc62d61
Artem Dudar <adudar@elevatecredit.co.uk>
2020-06-22 20:32:07 +0300

    fetch products types and brands

--- API/ClientApp/src/app/shop/shop.component.ts
+++ API/ClientApp/src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ShopService} from './shop.service';
 import {IProduct} from '../models/product';
+import {IBrand} from "../models/brand";
+import {IType} from "../models/product-type";
 
 @Component({
   selector: 'app-shop',
@@ -9,14 +11,44 @@ import {IProduct} from '../models/product';
 })
 export class ShopComponent implements OnInit {
   public products: IProduct[];
+  public brands: IBrand[];
+  public types: IType[];
 
-  constructor(private shopService: ShopService) { }
+  constructor(private shopService: ShopService) {
+  }
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+
+  }
+
+  getProducts() {
     this.shopService.getProducts()
       .subscribe(response => {
         this.products = response.data;
       }, (e) => console.error(e));
   }
+
+  getBrands() {
+    this.shopService.getBrands()
+      .subscribe((response: IBrand[]) => {
+        this.brands = response;
+      }, e => {
+        console.error(e);
+      });
+  }
+
+
+  getTypes() {
+    this.shopService.getTypes()
+      .subscribe((response: IType[]) => {
+        this.types = response;
+      }, e => {
+        console.error(e);
+      });
+  }
+
 
 }
--- API/ClientApp/src/app/shop/shop.service.ts
+++ API/ClientApp/src/app/shop/shop.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {IPagination} from '../models/pagination';
 import {Observable} from 'rxjs';
+import {IBrand} from "../models/brand";
+import {IType} from "../models/product-type";
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +16,12 @@ export class ShopService {
   getProducts(): Observable<IPagination> {
     return this.http.get<IPagination>(`${this.baseUrl}products?pageSize=50`);
   }
+
+  getBrands(): Observable<IBrand[]> {
+    return this.http.get<IBrand[]>(`${this.baseUrl}products/brands`);
+  }
+  getTypes(): Observable<IType[]> {
+    return this.http.get<IType[]>(`${this.baseUrl}products/types`);
+  }
+
 }
