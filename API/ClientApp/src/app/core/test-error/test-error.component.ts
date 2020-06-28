5e477b56c28a815dc4aaa2b920fc8a423d15fc81
Artem Dudar <adudar@elevatecredit.co.uk>
2020-06-28 23:40:15 +0300

    create components for errors

---
+++ API/ClientApp/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.css']
+})
+export class TestErrorComponent implements OnInit {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+
+  }
+
+  get404Error() {
+    return this.http.get(`${this.baseUrl}products/42`)
+      .subscribe(response => {
+        console.log(response);
+      }, e => console.log(e));
+  }
+
+  get500Error() {
+    return this.http.get(`${this.baseUrl}buggy/servererror`)
+      .subscribe(response => {
+        console.log(response);
+      }, e => console.log(e));
+  }
+
+  get400Error() {
+    return this.http.get(`${this.baseUrl}buggy/badrequest`)
+      .subscribe(response => {
+        console.log(response);
+      }, e => console.log(e));
+  }
+
+  get400ValidationError() {
+    return this.http.get(`${this.baseUrl}products/fortytwo`)
+      .subscribe(response => {
+        console.log(response);
+      }, e => console.log(e));
+  }
+
+}
