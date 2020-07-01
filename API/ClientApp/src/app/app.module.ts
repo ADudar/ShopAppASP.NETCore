77e179fd4674f68f73b24e60f1bc6afc2523a494
Artem Dudar <Artem Dudar@DESKTOP-L5MAP66>
2020-07-01 17:22:00 +0300

    adding spinner

--- API/ClientApp/src/app/app.module.ts
+++ API/ClientApp/src/app/app.module.ts
@@ -6,9 +6,11 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CoreModule} from './core/core.module';
-import {HomeModule} from "./home/home.module";
-import {AppRoutingModule} from "./app-routing.module";
-import {ErrorInterceptor} from "./core/interceptors/error.interceptor";
+import {HomeModule} from './home/home.module';
+import {AppRoutingModule} from './app-routing.module';
+import {ErrorInterceptor} from './core/interceptors/error.interceptor';
+import {NgxSpinnerModule} from 'ngx-spinner';
+import {LoadingInterceptor} from './core/interceptors/loading.interceptors';
 
 
 @NgModule({
@@ -22,10 +24,12 @@ import {ErrorInterceptor} from "./core/interceptors/error.interceptor";
     BrowserAnimationsModule,
     CoreModule,
     HomeModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxSpinnerModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
   ],
   bootstrap: [AppComponent]
 })
