6e6dc663aeae0ee0a4c38bb7d6bd3dbe7fd1ce9e
Artem Dudar <Artem Dudar@DESKTOP-L5MAP66>
2020-07-05 23:12:30 +0300

    added basket model with uuid

---
+++ API/ClientApp/src/app/models/basket.ts
@@ -0,0 +1,22 @@
+import { v4 as uuid } from 'uuid';
+
+interface IBasket {
+  id: string;
+  items: IBasketItem[];
+}
+
+interface IBasketItem {
+  id: number;
+  productName: string;
+  price: number;
+  quantity: number;
+  pictureUrl: string;
+  brand: string;
+  type: string;
+}
+
+export class Basket implements IBasket {
+  id = uuid();
+  items: IBasketItem[];
+
+}
