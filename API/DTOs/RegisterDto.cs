d9507c4a139d7188bb77ed078f69abbfe41f774c
Artem Dudar <Artem Dudar@DESKTOP-L5MAP66>
2020-07-07 16:20:15 +0300

    check for dup email

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,10 +1,16 @@
-﻿namespace API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
 {
     public class RegisterDto
     {
-        
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        // [RegularExpression()] TODO: add password regexp
         public string Password { get; set; }
     }
 }
