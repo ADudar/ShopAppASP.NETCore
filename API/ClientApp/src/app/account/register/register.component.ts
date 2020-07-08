03a090484749b2d1dd05241c17cdf9cace389626
Artem Dudar <Artem Dudar@DESKTOP-L5MAP66>
2020-07-08 16:09:23 +0300

    register form

--- API/ClientApp/src/app/account/register/register.component.ts
+++ API/ClientApp/src/app/account/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AccountService} from '../account.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router) {
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+        displayName: ['', [Validators.required]],
+        email: ['', [Validators.required]],
+        password: ['', [Validators.required]],
+      }
+    );
+  }
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value)
+      .subscribe(r => {
+        this.router.navigateByUrl('/shop');
+      }, e => {
+        console.error(e);
+      });
+  }
 }
